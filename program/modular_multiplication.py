63fef15b81f96bb8a9d07737345461357eabe3d1
Pjurkowsky <mrutopiak127@gmail.com>
2023-06-21 13:44:29 +0200

    sprawko

--- program/modular_multiplication.py
+++ program/modular_multiplication.py
@@ -1,42 +1,33 @@
 import math
 
 
-def getBitCount(X):
-    if X == 0:
+def bit_counter(number):
+    if number == 0:
         return 1
 
     count = 0
-    while X > 0:
+    while number > 0:
+
         count += 1
-        X >>= 1
+        number >>= 1
     return count
 
 
-def createSubvectors(X, subvectorBitCount, subvectorCount):
+def create_binary_subvector_alg_gorodecky(number, nrOfBits, nrOfSubvectors):
 
     subvectors = []
-    for _ in range(subvectorCount):
-        subvector = [0] * subvectorBitCount
+    for _ in range(nrOfSubvectors):
+        subvector = [0] * nrOfBits
         subvectors.append(subvector)
 
-    for i in range(subvectorCount):
-        for j in range(subvectorBitCount - 1, -1, -1):
-            subvectors[i][j] = X % 2
-            X //= 2
+    for i in range(nrOfSubvectors):
+        for j in range(nrOfBits - 1, -1, -1):
+            subvectors[i][j] = number % 2
+            number //= 2
     return subvectors
 
 
-def printSubvectors(subvectors):
-
-    for subvector in subvectors:
-        print("[ ", end="")
-        for bit in subvector:
-            print(bit, end=" ")
-            print("]", end=" ")
-            print()
-
-
-def subvectorToDec(subvector):
+def binary_to_dec(subvector):
 
     dec = 0
     for i in range(len(subvector)):
@@ -44,56 +35,52 @@ def subvectorToDec(subvector):
     return dec
 
 
-def getMultResult(A, B, P):
+def modulo_multiplication(A, B, P):
 
-    bitsOfbiggerNumber = getBitCount(max(A, B))
-    subvectorBitCount = 3  # r
-    subvectorCount = math.ceil(bitsOfbiggerNumber / subvectorBitCount)  # delta
-    print(bitsOfbiggerNumber, subvectorBitCount)
+    bitsOfbiggerNumber = bit_counter(max(A, B))
+    nrOfBits = 3  # r
+    nrOfSubvectors = math.ceil(bitsOfbiggerNumber / nrOfBits)  # delta
 
-    subvectorsA = createSubvectors(A, subvectorBitCount, subvectorCount)
-    printSubvectors(subvectorsA)
+    subvectorsA = create_binary_subvector_alg_gorodecky(
+        A, nrOfBits, nrOfSubvectors)
 
-    subvectorsB = createSubvectors(B, subvectorBitCount, subvectorCount)
-    printSubvectors(subvectorsB)
+    subvectorsB = create_binary_subvector_alg_gorodecky(
+        B, nrOfBits, nrOfSubvectors)
 
     result = 0
 
     for i in range(len(subvectorsA)):
         for j in range(len(subvectorsB)):
 
-            A_iter = subvectorToDec(subvectorsA[i])
-            B_iter = subvectorToDec(subvectorsB[j])
+            A_iter = binary_to_dec(subvectorsA[i])
+            B_iter = binary_to_dec(subvectorsB[j])
 
             S_temp = A_iter * B_iter * int(math.pow(2,
-                                                    ((i + 1) + (j + 1) - 2) * subvectorBitCount)) % P
+                                                    ((i + 1) + (j + 1) - 2) * nrOfBits)) % P
 
             result += S_temp
 
-    print(result)
+    range_ = int(math.pow(2, 3 * nrOfSubvectors + nrOfSubvectors))
 
-    range_ = int(math.pow(2, 3 * subvectorCount + subvectorCount))
-
-    rangeBitCount = getBitCount(range_)
-    resultSubvectorCount = math.ceil(rangeBitCount / subvectorBitCount)
-    resultSubvectors = createSubvectors(result, subvectorBitCount,
-                                        resultSubvectorCount)
-    printSubvectors(resultSubvectors)
+    rangeBitCount = bit_counter(range_)
+    resultSubvectorCount = math.ceil(rangeBitCount / nrOfBits)
+    resultSubvectors = create_binary_subvector_alg_gorodecky(result, nrOfBits,
+                                                             resultSubvectorCount)
 
     power = 0
-    ans = 0
-    for i in range(subvectorCount + 1):
-        ans_temp = subvectorToDec(
+    finalResult = 0
+    for i in range(nrOfSubvectors + 1):
+        ans_temp = binary_to_dec(
             resultSubvectors[i]) * int(math.pow(2, power)) % P
-        power += subvectorBitCount
-        ans += ans_temp
-    if ans > P:
-        ans -= P
-    print(ans)
+        power += nrOfBits
+        finalResult += ans_temp
+    if finalResult > P:
+        finalResult -= P
+    print(finalResult)
 
 
-A = 45
-B = 15
-P = 47
+A = 35
+B = 77
+P = 53
 
-getMultResult(A, B, P)
+modulo_multiplication(A, B, P)
